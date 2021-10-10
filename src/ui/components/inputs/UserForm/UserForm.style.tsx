db46d0f8a6d835b84acc94fabc8b0f32a19b1506
eneassena <desenvolvedoreneas2000@gmail.com>
2021-10-10 02:07:51 -0300

    ajustes

--- src/ui/components/inputs/UserForm/UserForm.style.tsx
+++ src/ui/components/inputs/UserForm/UserForm.style.tsx
@@ -14,7 +14,7 @@ export const BaseGrid = styled('div')`
     }    
 `;
 
- 
+
 export const PaymentData = styled(BaseGrid)`
     grid-template-columns: repeat(3, 1fr);
     grid-template-areas: 
@@ -31,6 +31,7 @@ export const PaymentData = styled(BaseGrid)`
             'erro';
     }
 `;
+
 export const UserData = styled(BaseGrid)`
     grid-template-columns: repeat(3, 1fr);
     grid-template-areas: 
@@ -51,27 +52,6 @@ export const NewContactData = styled(BaseGrid)`
         'email email'
         'senha password-strength'
         'confirmar-senha password-strength';
- 
-export const UserData = styled(BaseGrid)`
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-areas: 
-      'nome nome nome'
-      'data-nascimento cpf telefone';
-
-    ${({ theme }) => theme.breakpoints.down('md')} {
-        grid-template-areas: 
-          'nome'
-          'data-nascimento'
-          'cpf'
-          'telefone';
-    }
-`;
-export const NewContactData = styled(BaseGrid)`
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-areas: 
-      'email email'
-      'senha password-strength'
-      'confirmar-senha password-strength';
     ${({ theme }) => theme.breakpoints.down('md')} {
         grid-template-areas: 
             'email'
