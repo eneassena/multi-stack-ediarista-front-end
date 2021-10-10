f7fd05fef383e713c7973be0a36288094462c4b4
Enéas Senna <50535733+eneassena@users.noreply.github.com>
2021-10-10 02:02:05 -0300

    Update FormSchemaService.ts

--- src/data/services/FormSchemaService.ts
+++ src/data/services/FormSchemaService.ts
@@ -1,5 +1,4 @@
 import * as yup from 'yup';
- 
 import { DateService } from './DateService';
 import { PaymentService } from './PaymentService';
 import { ValidationService } from './ValidationService';
@@ -38,40 +37,26 @@ export const FormSchemaService = {
                 }),
             })
             .defined();
- 
-import { ValidationService } from './ValidationService';
-import { DateService } from './DateService';
-
-export const FormSchemaService = {
-    useData() {
-        return yup.object().shape({
-            usuario: yup.object().shape({
-                nome_completo: yup.string().min(3, 'Digite seu nome completo'),
-                nascimento: yup
-                    .date()
-                    .transform(DateService.transformDate)
-                    .min(
-                        DateService.maxAdultBirthday(),
-                        'Digite uma data valida'
-                    )
-                    .max(
-                        DateService.minAdultBirthday(),
-                        'Proibido menores de idade'
-                    )
-                    .typeError('Digite uma data valida'),
-                cpf: yup
-                    .string()
-                    .test('cpf', 'CPF Invalido', ValidationService.cpf),
-                telefone: yup
-                    .string()
-                    .test(
-                        'telefone',
-                        'Telefone invalido',
-                        ValidationService.telefone
-                    ),
-            }),
-        });
-     payment() {
+    },
+    newContact() {
+        return yup
+            .object()
+            .shape({
+                usuario: yup.object().shape({
+                    email: yup.string().email('E-mail Inválido'),
+                    password: yup.string().min(5, 'Senha muito curta'),
+                    password_confirmation: yup
+                        .string()
+                        .min(5, 'Senha muito curta')
+                        .oneOf(
+                            [yup.ref('password'), null],
+                            'As senhas não estão iguais'
+                        ),
+                }),
+            })
+            .defined();
+    },
+    payment() {
         return yup
             .object()
             .shape({
