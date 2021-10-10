94782e24371fa7aa81868e869311ec06b89f1048
eneassena <desenvolvedoreneas2000@gmail.com>
2021-10-10 01:47:53 -0300

    Implementação de novos components de formulario relacionados com a parte de cartão de credito

--- src/data/services/FormSchemaService.ts
+++ src/data/services/FormSchemaService.ts
@@ -1,38 +1,111 @@
 import * as yup from 'yup';
+import { DateService } from './DateService';
+import { PaymentService } from './PaymentService';
+import { ValidationService } from './ValidationService';
 
 export const FormSchemaService = {
+    useData() {
+        return yup
+            .object()
+            .shape({
+                usuario: yup.object().shape({
+                    nome_completo: yup
+                        .string()
+                        .min(3, 'Digite seu nome completo'),
+                    nascimento: yup
+                        .date()
+                        .transform(DateService.transformDate)
+                        .min(
+                            DateService.maxAdultBirthday(),
+                            'Digite uma data valida'
+                        )
+                        .max(
+                            DateService.minAdultBirthday(),
+                            'Proibido menores de idade'
+                        )
+                        .typeError('Digite uma data valida'),
+                    cpf: yup
+                        .string()
+                        .test('cpf', 'CPF Invalido', ValidationService.cpf),
+                    telefone: yup
+                        .string()
+                        .test(
+                            'telefone',
+                            'Telefone invalido',
+                            ValidationService.telefone
+                        ),
+                }),
+            })
+            .defined();
+    },
     newContact() {
         return yup
-            .object().shape({
+            .object()
+            .shape({
                 usuario: yup.object().shape({
                     email: yup.string().email('E-mail Inválido'),
                     password: yup.string().min(5, 'Senha muito curta'),
                     password_confirmation: yup
-                        .string().min(5, 'Senha muito curta')
+                        .string()
+                        .min(5, 'Senha muito curta')
                         .oneOf(
                             [yup.ref('password'), null],
                             'As senhas não estão iguais'
-                        )
-                })
+                        ),
+                }),
             })
             .defined();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    },
+    payment() {
+        return yup
+            .object()
+            .shape({
+                pagamento: yup.object().shape({
+                    numero_cartao: yup
+                        .string()
+                        .test(
+                            'card_number',
+                            'Número de Cartão invalido',
+                            (value) => {
+                                return PaymentService.validate({
+                                    card_number: value as string,
+                                    card_holder_name: '',
+                                    card_expiration_date: '',
+                                    card_cvv: '',
+                                }).card_number;
+                            }
+                        ),
+                    nome_cartao: yup.string(),
+                    validade: yup
+                        .string()
+                        .test(
+                            'card_expiration_date',
+                            'Data de validação inválida',
+                            (value) => {
+                                return PaymentService.validate({
+                                    card_number: '',
+                                    card_holder_name: '',
+                                    card_expiration_date: value as string,
+                                    card_cvv: '',
+                                }).card_expiration_date;
+                            }
+                        ),
+                    codigo: yup
+                        .string()
+                        .test(
+                            'card_cvv',
+                            'Código de validação inválido',
+                            (value) => {
+                                return PaymentService.validate({
+                                    card_number: '',
+                                    card_holder_name: '',
+                                    card_expiration_date: '',
+                                    card_cvv: value as string,
+                                }).card_cvv;
+                            }
+                        ),
+                }),
+            })
+            .defined();
+    },
+};
--- src/ui/components/inputs/UserForm/UserForm.style.tsx
+++ src/ui/components/inputs/UserForm/UserForm.style.tsx
@@ -5,33 +5,63 @@ import { styled } from '@material-ui/core/styles';
 export const BaseGrid = styled('div')`
     display: grid;
     grid-auto-rows: auto;
-    gap: ${({theme}) => theme.spacing(2) + ' ' + theme.spacing(3)};
-    padding: ${( {theme } ) => '0 0 ' + theme.spacing(5)};
+    gap: ${({ theme }) => theme.spacing(2) + ' ' + theme.spacing(3)};
+    padding: ${({ theme }) => '0 0 ' + theme.spacing(5)};
     
-    ${({theme}) => theme.breakpoints.down('md')} {
+    ${({ theme }) => theme.breakpoints.down('md')} {
         grid-template-columns: 1fr;
-        gap: ${({theme}) => theme.spacing(3)};
+        gap: ${({ theme }) => theme.spacing(3)};
     }    
 `;
 
-export const NewContactData = styled(BaseGrid) `
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-areas: 
-      'email email'
-      'senha password-strength'
-      'confirmar-senha password-strength';
-
-    ${({theme}) => theme.breakpoints.down('md')} {
+export const PaymentData = styled(BaseGrid)`
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas: 
+        'numero  numero'
+        'nome nome'
+        'validade codigo'
+        'erro erro';
+    ${({ theme }) => theme.breakpoints.down('md')} {
         grid-template-areas: 
-          'email'
-          'senha'
-          'confirmar-senha'
-          'password-strength';
+            'numero'
+            'nome'
+            'validade'
+            'codigo'
+            'erro';
+    }
+`;
+export const UserData = styled(BaseGrid)`
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas: 
+        'nome nome nome'
+        'data-nascimento cpf telefone';
+
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        grid-template-areas: 
+            'nome'
+            'data-nascimento'
+            'cpf'
+            'telefone';
+    }
+`;
+export const NewContactData = styled(BaseGrid)`
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-areas: 
+        'email email'
+        'senha password-strength'
+        'confirmar-senha password-strength';
+
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        grid-template-areas: 
+            'email'
+            'senha'
+            'confirmar-senha'
+            'password-strength';
     }
 `;
 
-export const PictureSelection = styled(BaseGrid) `
-  grid-template-columns: 1fr;
+export const PictureSelection = styled(BaseGrid)`
+    grid-template-columns: 1fr;
     padding: 0;
 `;
 
